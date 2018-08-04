4a199e63ceb7fb2f44e009598879869d8dd63727
Salakar <mike.diarmid@gmail.com>
2018-08-05 00:26:35 +0100

    add support for new syntax plugins supported in RN56

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-module.exports = {
-  plugins: [
-	"syntax-async-functions",
-    "syntax-class-properties",
-    "syntax-flow",
-    "syntax-jsx",
-    "syntax-object-rest-spread",
-    "syntax-trailing-function-commas",
-  ]
-}
+module.exports = function preset() {
+  return {
+    plugins: [
+      "syntax-async-functions",
+      "syntax-class-properties",
+      "syntax-flow",
+      "syntax-jsx",
+      "syntax-object-rest-spread",
+      "syntax-trailing-function-commas",
+      "syntax-exponentiation-operator",
+      "syntax-async-generators",
+      "syntax-optional-chaining",
+    ]
+  }
+};
