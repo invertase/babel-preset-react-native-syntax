91c738425a9cd1b0963225e5545a3a2fa7a77f94
Salakar <mike.diarmid@gmail.com>
2018-09-03 16:15:19 +0100

    add preset option to control flow strip/comment behaviour

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-module.exports = function preset() {
+module.exports = function preset(api, options) {
   return {
     plugins: [
       "@babel/syntax-jsx",
       "@babel/syntax-flow",
+      ...(!!options.flow && options.flow === 'strip' ? ['@babel/transform-flow-strip-types'] : []),
+      ...(!!options.flow && options.flow === 'comment' ? ['@babel/transform-flow-comments'] : []),
       "@babel/syntax-class-properties",
       "@babel/syntax-async-generators",
       "@babel/syntax-optional-chaining",
