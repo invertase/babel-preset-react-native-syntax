e7877e7b3977c13b05b5aa58aaf414217039bdf5
Salakar <mike.diarmid@gmail.com>
2018-09-03 16:14:40 +0100

    added typescript syntax support - but only if a ts/tsx file

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ module.exports = function preset() {
     plugins: [
       "@babel/syntax-jsx",
       "@babel/syntax-flow",
-      "@babel/syntax-typescript",
       "@babel/syntax-class-properties",
       "@babel/syntax-async-generators",
       "@babel/syntax-optional-chaining",
       "@babel/syntax-object-rest-spread",
       "@babel/syntax-optional-catch-binding",
       "@babel/syntax-nullish-coalescing-operator"
-    ]
+    ],
+    overrides: [
+      {
+        test: $ => !!$ && ($.endsWith('.ts') || $.endsWith('.tsx')),
+        plugins: [
+          ['@babel/syntax-typescript', { isTSX: true }],
+        ],
+      },
+    ],
   }
 };
